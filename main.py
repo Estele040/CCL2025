424935c59e87eed87edf313661f452fbb8186bb1
Estele040 <tanghuiye1@163.com>
2025-03-08 16:52:04 +0800

    2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas
 import numpy
+import jieba
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
@@ -9,11 +10,9 @@ data_path = './date'
 train_file = './data/train.json'
 test_file = './data/test1.json'
 
-df = pd.read_json(train_file)
-print(df.keys())
-print(df['id'].shape)
-print(df['content'].shape)
-print(df['output'].shape)
+df_train = pd.read_json(train_file)
+df_test = pd.read_json(test_file)
+
 
 hate = 'hate'
 Nhate = 'non-hate'
@@ -37,13 +36,15 @@ def parse_output(output):
             })
     return results
 
-solved_output = list(map(parse_output, df['output']))
-print(solved_output[0])
+solved_train_output = list(map(parse_output, df_train['output']))
+
+
+print(solved_train_output[0])
 
 # 构建结构化数据集
-def build_structured_data(df):
+def build_structured_data(df_train):
     structured_data = []
-    for idx, row in df.iterrows():
+    for idx, row in df_train.iterrows():
         content = row['content']
         output = row['output']
         quadruples = parse_output(output)
@@ -59,8 +60,10 @@ def build_structured_data(df):
     return pd.DataFrame(structured_data)
 
 # 示例
-structured_df = build_structured_data(df)
-print(structured_df.columns)
+train_structured_df = build_structured_data(df_train)
+
+print(train_structured_df.columns)
+
 
 def is_hate(output_data):
     hate_count = 0
@@ -73,15 +76,45 @@ def is_hate(output_data):
     return [hate_count, Nhate_count]
 
 # 示例
-print('hate_count:', is_hate(structured_df)[0])
-print('non-hate_count:', is_hate(structured_df)[1])
+print('hate_count:', is_hate(train_structured_df)[0])
+print('non-hate_count:', is_hate(train_structured_df)[1])
+
 
 #数据编码
 label_encoder_group = LabelEncoder()
 label_encoder_hateful = LabelEncoder()
 
-structured_df['Targeted Group Encoded'] = label_encoder_group.fit_transform(structured_df['Targeted Group'])
-structured_df['Hateful Encoded'] = label_encoder_hateful.fit_transform(structured_df['Hateful'])
+train_structured_df['Targeted Group Encoded'] = label_encoder_group.fit_transform(train_structured_df['Targeted Group'])
+train_structured_df['Hateful Encoded'] = label_encoder_hateful.fit_transform(train_structured_df['Hateful'])
 
-print(structured_df[['Targeted Group', 'Targeted Group Encoded']].drop_duplicates())
-print(structured_df[['Hateful', 'Hateful Encoded']].drop_duplicates())
+
+
+print(train_structured_df[['Targeted Group', 'Targeted Group Encoded']].drop_duplicates())
+print(train_structured_df[['Hateful', 'Hateful Encoded']].drop_duplicates())
+
+
+print(train_structured_df.isnull().sum())
+print(train_structured_df.duplicated().sum())
+
+from sklearn.preprocessing import MultiLabelBinarizer
+import pandas as pd
+import jieba
+
+# 假设 structured_df 已经正确定义
+# structured_df = ...
+
+# 多标签编码
+mlb = MultiLabelBinarizer()
+targeted_group_encoded = mlb.fit_transform(train_structured_df['Targeted Group'].str.split(', '))
+targeted_group_df = pd.DataFrame(targeted_group_encoded, columns=mlb.classes_)
+train_structured_df = pd.concat([train_structured_df, targeted_group_df], axis=1)
+
+# 分词
+train_structured_df['content_tokenized'] = train_structured_df['content'].apply(lambda x: ' '.join(jieba.lcut(x)))
+train_structured_df['Target_tokenized'] = train_structured_df['Target'].apply(lambda x: ' '.join(jieba.lcut(x)))
+train_structured_df['Argument_tokenized'] = train_structured_df['Argument'].apply(lambda x: ' '.join(jieba.lcut(x)))
+
+
+# 打印结果
+print(train_structured_df.head())
+
